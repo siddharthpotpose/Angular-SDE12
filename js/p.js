cbeb9bcd6c8f7918121a9b5c5c391d0d4067af29
siddharthpotpose <siddharthpotpose9@gmail.com>
2022-10-23 02:17:37 +0530

    new update

--- js/p.js
+++ js/p.js
@@ -12,3 +12,78 @@ while(num3<=20){
     console.log(num3)
     num3=num3+2
 }
+// -------------------------------------------------------------
+// simple function 
+ function fun(){
+    console.log("hello")
+ }
+ fun()
+//  -----------------------------------------------
+// parametarised / argument function 
+
+
+function sid(num1,num2){
+    ans=num1+num2
+    console.log(ans)
+}
+sid(5,5)
+// ------------------------------------------
+// return function 
+
+function mul(num3,num4){
+    return num3*num4
+}
+let total=mul(50,2)
+console.log(total)
+// ----------------------------------------------
+
+// print a odd numbers using function 
+ function table(t){
+    for(t;t<=10;t=t+2)
+    console.log(t)
+ }
+ table(1)
+
+
+// -------------------------------------------------------
+
+function ss(c){
+    for(c;c<=10;c++)
+    console.log(c*2)
+}
+ss(1)
+// -----------------------------------------
+
+// let num7="yes";
+// do{
+//     num7=prompt("do you want continue yes/no")
+//     console.log("yes/no"+num7)
+// }while(num7==="yes")
+// // --------------------------------------------------------
+// alert("hello")
+// ---------------------------------------------------------------
+// document.write("happy diwali")
+// ---------------------------------------------------------------
+let pp=prompt("enter number ");
+let dp=prompt("enter number");
+let sp=pp-dp
+document.write("addition of two number ="+sp)
+// ------------------------------------------------------------------
+  function arpan(){
+
+    let math=document.getElementById("num1").value
+    let sci=document.getElementById("num2").value
+    let result=math-sci
+    document.write("substraction ="+result)
+
+  }
+
+
+
+
+
+
+
+
+
+
